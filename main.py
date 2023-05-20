e8a19cdd4236821cd0cb20e90f6c40569b65e7f7
petrKantek <57535586+petrKantek@users.noreply.github.com>
2023-05-20 23:13:58 +0200

    make api async

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
-from azure.servicebus import ServiceBusClient, ServiceBusMessage
+from azure.servicebus import ServiceBusMessage
+from azure.servicebus.aio import ServiceBusClient
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
@@ -17,14 +18,14 @@ class Order(BaseModel):
     products: list[Product] = Field(..., min_items=1)
 
 @app.post("/buy")
-def buy_product(order: Order):
+async def buy_product(order: Order):
     # Send a message to Azure Service Bus
     conn_str = os.getenv("SERVICE_BUS_CONNECTION_STRING")
     queue_name = "invoices"
     message = ServiceBusMessage(order.json())
 
-    with ServiceBusClient.from_connection_string(conn_str) as client:
-        with client.get_queue_sender(queue_name) as sender:
-            sender.send_messages(message)
+    async with ServiceBusClient.from_connection_string(conn_str) as client:
+        async with client.get_queue_sender(queue_name) as sender:
+            await sender.send_messages(message)
 
     return {"message": "Product purchase request sent."}
